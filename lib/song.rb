b1c52d46e04bb6ddd64e55c692566734c30aa734
Anthony Polito <anthonyalexanderpolito@gmail.com>
2020-08-12 17:30:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,11 @@
-class Song
-
-  attr_accessor :name, :artist, :genre
-
-  @@count = 0
-  @@artists = []
-  @@genres = []
-
-  def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre
-    @@genres << genre
-    @@artists << artist
+def self.genre_count
+  genre_count = {}
+  @@genres.each do |genre|
+    if genre_count[genre]
+      genre_count[genre] += 1
+    else
+      genre_count[genre] = 1
+    end
   end
-
+  genre_count
 end
